bc37a01777c53ec8eb0114b33ac228e8ead09461
zdeik <zdeik@naver.com>
2025-08-18 16:46:00 +0900

    dash

---
+++ 72.dash_test.py
@@ -0,0 +1,37 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+
+#앱 생성
+app = Dash(__name__)
+
+#레이아웃 정의
+app.layout = html.Div([
+    html.H4('Interactive scatter plot with Iris dataset'),
+    dcc.Graph(id="scatter-plot"),
+    html.P("Filter by petal width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=2.5, step=0.1,
+        marks={0: '0', 2.5: '2.5'},
+        value=[0.5, 2]
+    ),
+])
+
+
+@app.callback(
+    Output("scatter-plot", "figure"),
+    Input("range-slider", "value"))
+def update_bar_chart(slider_range):
+    df = px.data.iris() # replace with your own data source
+    low, high = slider_range
+    mask = (df['petal_width'] > low) & (df['petal_width'] < high)
+    fig = px.scatter(
+        df[mask], x="sepal_width", y="sepal_length",
+        color="species", size='petal_length',
+        hover_data=['petal_width'])
+    return fig
+
+#서버 실행
+app.run(debug=True)
+
+#python app.py
