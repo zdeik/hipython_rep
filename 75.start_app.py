edf464fa77038e57d23ff38954ceaac1c98443f9
zdeik <zdeik@naver.com>
2025-08-20 10:30:25 +0900

    streamlit

--- 75.start_app.py
+++ 75.start_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+
 st.title('스트림릿 안녕하세요')
-name = st.text_input('이름:')
-st.write(f"안녕하세요, {name}님!")
+
+st.divider()
+
+name = st.text_input('이름 :')
+if name:
+    st.write(f"안녕하세요, {name}님!")
+    
+import pandas as pd
+df = pd.read_csv('./data/ABNB_stock.csv')
+df
+
---
+++ 76.input.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('스트림릿 안녕하세요')
+
+st.header('haeder')
+st.subheader('subheader')
